1a1772f0e902b9f7c9435b2fa1869271d926e285
Olivia99 <jieying.yang@cca.edu>
2019-11-18 21:28:19 -0800

    add a pin and add zoom in feature

--- GoogleMap/ViewController.swift
+++ GoogleMap/ViewController.swift
@@ -12,9 +12,11 @@ import CoreLocation
 let regionInMeters = 10000
 
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+var previousLocation : CLLocation?
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     let locationManeger = CLLocationManager()
     
     
@@ -56,11 +58,8 @@ class ViewController: UIViewController {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             // do map stuff
-            mapView.showsUserLocation = true
-            centerViewOfUserLocation()
-            locationManeger.startUpdatingLocation()
-            
-            break
+           
+         startTrackingUserLocation()
             
         case.denied:
             break
@@ -78,26 +77,82 @@ class ViewController: UIViewController {
     }
 
     }
+    
+    func startTrackingUserLocation(){
+        mapView.showsUserLocation = true
+                   centerViewOfUserLocation()
+                   locationManeger.startUpdatingLocation()
+                   previousLocation = getCenterLocation(for: mapView)
+                   
+    }
+    
+    func getCenterLocation(for mapView: MKMapView)->CLLocation{
+        
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 }
+
 
 extension ViewController: CLLocationManagerDelegate{
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else{return}
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 10000, longitudinalMeters: 10000)
-        
-        mapView.setRegion(region, animated: true)
-        
-        
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else{return}
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: 10000, longitudinalMeters: 10000)
+//
+//        mapView.setRegion(region, animated: true)
+//
+//
+//    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
 checkLocationAuthorization()
     }
     
+   
+    }
+
+
+
+extension ViewController: MKMapViewDelegate{
+       
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+     let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard var previousLocation = self.previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) < 50 else { return }
+        previousLocation = center
+        geoCoder.reverseGeocodeLocation(center){[weak self] (placemarks, error) in
+            guard let self = self else{return}
+            
+            if let _ = error{
+                return
+            }
+            
+            guard let placemark = placemarks?.first else{
+                return
+            }
+            
+            
+                let streetNumber = placemark.subThoroughfare ?? ""
+          let streetName = placemark.thoroughfare ?? ""
+                
+                DispatchQueue.main.sync {
+                    self.addressLabel.text = "\(streetNumber) \(streetName)"
+                }
+        }
+    }
+      
+    
+    
     
 }
+
 
 
